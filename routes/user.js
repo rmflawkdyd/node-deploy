e6869474a430b2d2d3c07a2f3bbd44e42a6c2d43
rmflawkdyd <mytjdcjf@gmail.com>
2021-03-24 17:36:58 +0900

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const { isLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+const { addFollowing } = require('../controllers/user');
+
+const router = express.Router();
+
+router.post('/:id/follow',isLoggedIn,addFollowing);
+
+module.exports = router;
